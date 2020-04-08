499bbea0ebfda0008a9dd36201c07a4ee2b35127
tpiros <2042718+tpiros@users.noreply.github.com>
2020-04-09 00:28:34 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Video Test</p>
+    <video id="doc-player" controls  muted  class="cld-video-player cld-fluid"></video>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import cloudinary from 'cloudinary-core';
+import "../node_modules/cloudinary-video-player/dist/cld-video-player.min.js";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  mounted: () => {
+    const cld = cloudinary.Cloudinary.new({ cloud_name: 'tamas-demo' });
+    const demoplayer = cld.videoPlayer('doc-player').width(600);
+    demoplayer.source('web-perf/IMG_0460')
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "../node_modules/cloudinary-video-player/dist/cld-video-player.min.css"
+
 Vue.config.productionTip = false
 
 new Vue({
